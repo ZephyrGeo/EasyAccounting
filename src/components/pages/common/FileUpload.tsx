4e8be9a4512252bf3b261423914998b4efd3c8e8
Muelsyse <33949957+ZephyrGeo@users.noreply.github.com>
2025-12-14 01:18:37 +0900

    Revert "Feature/20251214/replace icon title"

--- src/components/pages/common/FileUpload.tsx
+++ src/components/pages/common/FileUpload.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Upload } from "lucide-react";
 
-export default function BillImport() {
+export default function FileUpload() {
   return (
     <Button className="">
       <Upload />
-      Bill Import
+      Bill Upload
     </Button>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/claude-api": {
-        target: "https://api.anthropic.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/claude-api/, ""),
-        headers: {
-          Origin: "https://api.anthropic.com",
-        },
-      },
     },
   },
 });
